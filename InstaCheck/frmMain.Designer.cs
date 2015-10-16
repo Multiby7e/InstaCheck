46474d8f230c8f82e0738f4939aff96780d39540
Multiby7e <predatorx1990@gmail.com>
2015-10-16 17:09:22 +0200

    InstaCheck
    A simple Instagram Checker

---
+++ InstaCheck/frmMain.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace InstaCheck
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.tabMain = new System.Windows.Forms.TabControl();
+            this.tabUsernames = new System.Windows.Forms.TabPage();
+            this.listUsernames = new System.Windows.Forms.ListView();
+            this.colUsername = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colStatus = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuSettings = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnBot = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadUsernamesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnLoad = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnClear = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnExport = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabCredits = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabMain.SuspendLayout();
+            this.tabUsernames.SuspendLayout();
+            this.menuSettings.SuspendLayout();
+            this.tabCredits.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabMain
+            // 
+            this.tabMain.Controls.Add(this.tabUsernames);
+            this.tabMain.Controls.Add(this.tabCredits);
+            this.tabMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabMain.Location = new System.Drawing.Point(0, 0);
+            this.tabMain.Name = "tabMain";
+            this.tabMain.SelectedIndex = 0;
+            this.tabMain.Size = new System.Drawing.Size(374, 192);
+            this.tabMain.TabIndex = 32;
+            // 
+            // tabUsernames
+            // 
+            this.tabUsernames.Controls.Add(this.listUsernames);
+            this.tabUsernames.Location = new System.Drawing.Point(4, 22);
+            this.tabUsernames.Name = "tabUsernames";
+            this.tabUsernames.Padding = new System.Windows.Forms.Padding(3);
+            this.tabUsernames.Size = new System.Drawing.Size(366, 166);
+            this.tabUsernames.TabIndex = 0;
+            this.tabUsernames.Text = "Usernames";
+            this.tabUsernames.UseVisualStyleBackColor = true;
+            // 
+            // listUsernames
+            // 
+            this.listUsernames.BackColor = System.Drawing.Color.White;
+            this.listUsernames.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listUsernames.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colUsername,
+            this.colStatus});
+            this.listUsernames.ContextMenuStrip = this.menuSettings;
+            this.listUsernames.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listUsernames.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listUsernames.ForeColor = System.Drawing.Color.Black;
+            this.listUsernames.FullRowSelect = true;
+            this.listUsernames.GridLines = true;
+            this.listUsernames.Location = new System.Drawing.Point(3, 3);
+            this.listUsernames.Name = "listUsernames";
+            this.listUsernames.Size = new System.Drawing.Size(360, 160);
+            this.listUsernames.TabIndex = 32;
+            this.listUsernames.UseCompatibleStateImageBehavior = false;
+            this.listUsernames.View = System.Windows.Forms.View.Details;
+            // 
+            // colUsername
+            // 
+            this.colUsername.Text = "Username";
+            this.colUsername.Width = 181;
+            // 
+            // colStatus
+            // 
+            this.colStatus.Text = "Status";
+            this.colStatus.Width = 177;
+            // 
+            // menuSettings
+            // 
+            this.menuSettings.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnBot,
+            this.loadUsernamesToolStripMenuItem});
+            this.menuSettings.Name = "menuSettings";
+            this.menuSettings.Size = new System.Drawing.Size(133, 48);
+            // 
+            // btnBot
+            // 
+            this.btnBot.Name = "btnBot";
+            this.btnBot.Size = new System.Drawing.Size(132, 22);
+            this.btnBot.Text = "Start";
+            this.btnBot.Click += new System.EventHandler(this.btnBot_Click);
+            // 
+            // loadUsernamesToolStripMenuItem
+            // 
+            this.loadUsernamesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnLoad,
+            this.btnClear,
+            this.btnExport});
+            this.loadUsernamesToolStripMenuItem.Name = "loadUsernamesToolStripMenuItem";
+            this.loadUsernamesToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.loadUsernamesToolStripMenuItem.Text = "Usernames";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(107, 22);
+            this.btnLoad.Text = "Load";
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(107, 22);
+            this.btnClear.Text = "Clear";
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(107, 22);
+            this.btnExport.Text = "Export";
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // tabCredits
+            // 
+            this.tabCredits.Controls.Add(this.label2);
+            this.tabCredits.Controls.Add(this.label1);
+            this.tabCredits.Location = new System.Drawing.Point(4, 22);
+            this.tabCredits.Name = "tabCredits";
+            this.tabCredits.Size = new System.Drawing.Size(366, 166);
+            this.tabCredits.TabIndex = 2;
+            this.tabCredits.Text = "Credits";
+            this.tabCredits.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(342, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "www.hackforums.net";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(77, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(203, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Coded by: Multibyte";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 170);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(374, 22);
+            this.statusStrip1.TabIndex = 33;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(57, 17);
+            this.lblStatus.Text = "Waiting...";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 192);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(390, 230);
+            this.Name = "frmMain";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "InstaCheck - A simple Instagram checker";
+            this.tabMain.ResumeLayout(false);
+            this.tabUsernames.ResumeLayout(false);
+            this.menuSettings.ResumeLayout(false);
+            this.tabCredits.ResumeLayout(false);
+            this.tabCredits.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabMain;
+        private System.Windows.Forms.TabPage tabUsernames;
+        internal System.Windows.Forms.ColumnHeader colUsername;
+        internal System.Windows.Forms.ColumnHeader colStatus;
+        private System.Windows.Forms.TabPage tabCredits;
+        private System.Windows.Forms.ContextMenuStrip menuSettings;
+        private System.Windows.Forms.ToolStripMenuItem btnBot;
+        private System.Windows.Forms.ToolStripMenuItem loadUsernamesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem btnLoad;
+        private System.Windows.Forms.ToolStripMenuItem btnClear;
+        private System.Windows.Forms.ToolStripMenuItem btnExport;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        internal System.Windows.Forms.ListView listUsernames;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ InstaCheck/frmMain.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace InstaCheck //Created by Multibyte --- Hackforums.net
+{
+    public partial class frmMain : Form
+    {
+        public static int nThreads = 25;
+        public static bool Working = false;
+
+        public static Thread[] MainThread = new Thread[1];
+        public static Dictionary<int, int> ThreadStart = new Dictionary<int, int>();
+        public static Dictionary<int, int> ThreadEnd = new Dictionary<int, int>();
+
+        public static List<string> Usernames = new List<string>();
+        public static List<string> WorkingUsernames = new List<string>();
+
+        public frmMain()
+        {
+            InitializeComponent();
+            MessageBox.Show("InstaCheck - Coded by Multibyte (Hackforums.net)");
+            CheckForIllegalCrossThreadCalls = false;
+            listUsernames.Sorting = SortOrder.Ascending;
+        }
+        static int EachCount = 0;
+        private void btnBot_Click(object sender, EventArgs e)
+        {
+            if (!Working)
+            {
+                int Count = Usernames.Count;
+                EachCount = Count / nThreads;
+                int CurCount = 1;
+                for (int i = 1; i < nThreads + 1; i++)
+                {
+                    ThreadStart[i - 1] = CurCount;
+                    ThreadEnd[i - 1] = EachCount * i;
+                    CurCount = EachCount * i;
+                }
+                
+                MainThread = new Thread[nThreads];
+                StartThreads(nThreads);
+                btnBot.Text = "Stop";
+                lblStatus.Text = "Checker started.";
+                Working = true;
+            }
+            else
+            {
+                StopThreads();
+                btnBot.Text = "Start";
+                lblStatus.Text = "Waiting...";
+                Working = false;
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Ofd = new OpenFileDialog();
+            Ofd.Filter = "Text files|*.txt";
+            if (Ofd.ShowDialog() == DialogResult.OK)
+            {
+                string[] Lines = File.ReadAllLines(Ofd.FileName);
+                foreach (string Line in Lines)
+                {
+                    Usernames.Add(Line);
+                }
+            }
+            nThreads = Usernames.Count / 2;
+            lblStatus.Text = "Loaded " + Usernames.Count.ToString() + " usernames.";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listUsernames.Items.Clear();
+        }
+
+        public void StartThreads(int nThreads = 1)
+        {
+            for (int i = 0; i < nThreads; i++)
+            {
+                MainThread[i] = new Thread(() => MainMethod(i, listUsernames));
+                MainThread[i].Name = String.Format("Working Thread: {0}", i);
+                MainThread[i].IsBackground = true;
+                MainThread[i].Start();
+            }
+        }
+
+        public static void StopThreads()
+        {
+            foreach (Thread Thread in MainThread)
+            {
+                Thread.Abort();
+            }
+        }
+
+        public static string GetHTML(string Url)
+        {
+            HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(Url);
+            WebHeaderCollection getHeaders = myRequest.Headers;
+            myRequest.Method = "GET";
+            WebResponse myResponse = myRequest.GetResponse();
+            StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+            string HTML = sr.ReadToEnd();
+            sr.Close();
+            myResponse.Close();
+            return HTML;
+        }
+
+        public static bool Exists(string Username)
+        {
+            try
+            {
+                if (GetHTML("https://instagram.com/" + Username).Contains(Username))
+                    return true;
+                else
+                    return false;
+            }
+            catch { return false; }
+        }
+
+        public static void MainMethod(int ThreadIndex, ListView MyList)
+        {
+            for (int i = ThreadStart[ThreadIndex - 1]; i < ThreadEnd[ThreadIndex - 1]; i++)
+            {
+                bool isExisting = Exists(Usernames[i]);
+                if (!isExisting)
+                    AddAvailable(Usernames[i], MyList);
+            }
+        }
+
+        public static void AddAvailable(string Username, ListView MyList)
+        {
+            string[] Row = { Username, "Available" };
+            var Item = new ListViewItem(Row);
+            if (!WorkingUsernames.Contains(Username))
+            {
+                WorkingUsernames.Add(Username);
+                MyList.Items.Add(Item);
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            using (var MyStream = new StreamWriter(Application.StartupPath + "/Usernames.txt"))
+            {
+                foreach (ListViewItem Item in listUsernames.Items)
+                {
+                    MyStream.WriteLine(Item.Text);
+                }
+            }
+        }
+    }
+}
